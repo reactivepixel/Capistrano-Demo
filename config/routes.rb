59c86d8ea3373a5834f24b0a5f939426c0e49e31
Chapman <chapman@apextion.com>
2015-02-26 22:34:14 -0500

    dummy scaffolding in place. This makes our basic app we will deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :things
+  root "things#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
